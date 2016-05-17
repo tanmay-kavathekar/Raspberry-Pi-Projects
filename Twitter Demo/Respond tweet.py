56a5bc8e0209b1a5652bcdaf374f74eeee9c40cb
Tanmay Kavathekar <tkavath@g.clemson.edu>
2016-05-17 18:27:19 -0400

    Twitter demo using twython

--- Twitter Demo/Respond tweet.py
+++ Twitter Demo/Respond tweet.py
@@ -1,9 +1,20 @@
-from twython import Twython
+from twython import TwythonStreamer
+
+exec(open('int_key.py').read())
+
+count=0
+
 class MyStreamer(TwythonStreamer):
 	def on_success(self,data):
+		global count
 		if 'text' in data:
-			print("Found "+"text")
+			count=count+1
+			print(count)
+			if count==3:
+				print("Ian G. Harris is popular!")
+				self.disconnect()
 
-stream=MyStreamer(C_KEY,C_SECRET,A_TOKEN,A_SECRET)
+stream=MyStreamer(C_KEY,C_S_KEY,A_TOK,A_S_TOK)
 
-stream.statuses.filter(track="xyz")
+stream.statuses.filter(track="Ian G. Harris")
+	
